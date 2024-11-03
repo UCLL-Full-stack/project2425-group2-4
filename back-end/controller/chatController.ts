38c4761f1d4fc1f2a1ecb530292213f5f1a5e956
Nassim Madrane <nassim.madrane@proton.me>
2024-11-03 13:54:51 +0100

    fixed error handling a little bit

--- back-end/controller/chatController.ts
+++ back-end/controller/chatController.ts
@@ -20,10 +20,12 @@ const getChats = async (req: Request, res: Response) => {
     try {
 
         await db.authenticate(token);
+        db.select('chat')
+            .then((chats) => { db.close; res.json(chats); })
+            .catch((e) => { db.close(); res.status(500).json({ message: 'Error fetching chats.' }); });
 
-    } catch (e) { db.close; res.json({ message: 'Error authenticating.' }); }
+    } catch (e) { res.status(500).json({ message: 'Error authenticating.' }); }
 
-    db.select('chat').then((chats) => { db.close; res.json(chats); }).catch((e) => { db.close(); res.json({ message: 'Error fetching chats.' }); });
 
 };
 
@@ -34,35 +36,41 @@ const createChat = async (req: Request, res: Response) => {
     const { token, name } = req.body;
     const db = new Surreal();
     await db.connect(url, { namespace: namespace, database: database });
-    await db.authenticate(token).catch((e) => { db.close(); res.json({ message: 'Error authenticating.' }); });
+    try {
+        await db.authenticate(token);
+        let user: user = new Object() as user;
 
-    let user: user = new Object() as user;
+        await db.query<[user]>('SELECT * FROM $auth.id').then((u) => user = u[0]).catch((e) => { db.close(); res.status(500).json({ message: 'Error fetching user' }) }).finally(() => console.log({ user: user }));
 
-    await db.query<[user]>('SELECT * FROM $auth.id').then((u) => user = u[0]).catch((e) => { db.close(); res.json({ message: 'Error fetching user' }) }).finally(() => console.log({ user: user }));
+        let newChat: chat[] = await db.create<chat>('chat', { name: name }).catch((e) => { db.close(); res.status(500).json({ message: 'Error creating chat' }) }) || new Object() as Array<chat>;
 
-    let newChat: chat[] = await db.create<chat>('chat', { name: name }).catch((e) => { db.close(); res.json({ message: 'Error creating chat' }) }) || new Object() as Array<chat>;
+        await db.query('RELATE $chat->hasUser->$auth;', { chat: newChat[0] }).catch((e) => { db.close; res.status(500).json({ message: 'Error allocating user to new chat' }) });
 
-    await db.query('RELATE $chat->hasUser->$auth;', { chat: newChat[0] }).catch((e) => { db.close; res.json({ message: 'Error allocating user to new chat' }) });
+        await db.query('SELECT *,->hasUser->user.{username,id,email,role} as users, <-writtenIn<-message.{ text,id,timestamp,messenger.{username,email,role}} as messages from $fd', { fd: newChat[0] })
+            .then((chat) => { db.close(); res.json(chat) })
+            .catch((e) => { db.close(); res.status(500).json({ message: 'Error fetching chat' }) })
 
-    await db.query('SELECT *,->hasUser->user.{username,id,email,role} as users, <-writtenIn<-message.{ text,id,timestamp,messenger.{username,email,role}} as messages from $fd', { fd: newChat[0] })
-        .then((chat) => { db.close(); res.json(chat) })
-        .catch((e) => { db.close(); res.json({ message: 'Error fetching chat' }) })
+    } catch (e) { db.close(); res.status(500).json({ message: 'Error authenticating.' }); }
 };
 
 const getChat = async (req: Request, res: Response) => {
     const namespace = process.env.DBNAMESPACE || "";
     const database = process.env.DBDATABASE || "";
     const url = process.env.DBURL || "";
+
     const { token } = req.body;
     const { chatId } = req.params;
+
     console.log(namespace, database, url, chatId);
     const db = new Surreal();
     await db.connect(url, { namespace: namespace, database: database });
-    db.authenticate(token).then(() => { }).catch((e) => { db.close(); res.json({ message: 'Error authenticating.' }); });
+    try {
+        await db.authenticate(token);
 
-    await db.query('SELECT *,->hasUser->user.{username,id,email,role} as users, <-writtenIn<-message.{ text,id,timestamp,messenger.{username,email,role}} as messages from $fd', { fd: new StringRecordId(chatId) })
-        .then((chat) => { db.close(); res.json(chat) })
-        .catch((e) => { db.close(); res.json({ message: 'Error fetching chat' }) })
+        await db.query('SELECT *,->hasUser->user.{username,id,email,role} as users, <-writtenIn<-message.{ text,id,timestamp,messenger.{username,email,role}} as messages from $fd', { fd: new StringRecordId(chatId) })
+            .then((chat) => { db.close(); res.json(chat) })
+            .catch((e) => { db.close(); res.status(500).json({ message: 'Error fetching chat' }) })
+    } catch (e) { db.close(); res.status(500).json({ message: 'Error authenticating' }) }
 };
 
 const sendMessage = async (req: Request, res: Response) => {
@@ -74,12 +82,14 @@ const sendMessage = async (req: Request, res: Response) => {
     const db = new Surreal();
     await db.connect(url, { namespace: namespace, database: database });
 
-    db.authenticate(token).then(() => { }).catch((e) => { db.close(); res.json({ message: 'Error authenticating.' }); });
-    let message: message[] = await db.create<message>('message', { text: messageContent }).catch(() => { db.close(); res.json({ message: "Error creating new message" }) }) || new Object() as Array<message>;
+    try {
+        db.authenticate(token);
+        let message: message[] = await db.create<message>('message', { text: messageContent }).catch((e) => { db.close(); res.status(500).json({ message: "Error creating new message" }) }) || new Object() as Array<message>;
 
-    await db.query('RELATE $message->writtenIn->$chat;', { message: message[0], chat: new StringRecordId(chatId) }).catch(() => { db.close(); res.json({ message: "Error relating message to chat" }) });
+        await db.query('RELATE $message->writtenIn->$chat;', { message: message[0], chat: new StringRecordId(chatId) }).catch(() => { db.close(); res.status(500).json({ message: "Error relating message to chat" }) });
 
-    res.json(message);
+        res.json(message);
+    } catch (e) { res.status(500).json({ message: "Error authenticating" }) }
 
 };
 
--- back-end/routes/authRouter.ts
+++ back-end/routes/authRouter.ts
@@ -7,5 +7,4 @@ const router = Router();
 router.post('/login', authController.login);
 router.post('/logout', authController.logout);
 router.post('/register', authController.register);
-
 module.exports = router;
